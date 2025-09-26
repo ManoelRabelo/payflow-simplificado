7e086578705b4e853d7f50a9f7283933dda22e41
Manoel <manoelrabelo7@gmail.com>
2025-09-25 21:51:56 -0300

    chore: configure H2 of UserService

---
+++ user-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_users(name, document, email, balance, password, type)
+VALUES ('Meggie', '11122233344', 'meggie@email.com', 100.00, '123456', 'COMMON');
+INSERT INTO tb_users(name, document, email, balance, password, type)
+VALUES ('Tigresa', '12312312312', 'tigresa@email.com', 100.00, '123456', 'COMMON');
+INSERT INTO tb_users(name, document, email, balance, password, type)
+VALUES ('Jack', '12345678900', 'jack@email.com', 100.00, '123456', 'MERCHANT');
